56218e26cf9a070861cdc15edc9e0af8095bd72c
EvanMarshalsey <84351307+EvanMarshalsey@users.noreply.github.com>
2021-06-16 10:57:46 +0100

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,13 +1,11 @@
 (function () {
-
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
   for (var name in names) {  
     var firstLetter = names[name].charAt(0).toLowerCase();
-
     if (firstLetter == "j") {
       byeSpeaker(names[name]);
-    } else {
+    } 
+    else {
       helloSpeaker(names[name]);
     }
    }
